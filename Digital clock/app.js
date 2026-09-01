b4b81a9076e6ccd670315c6a256f4196d88aad13
Kharate Gaurav <sachinkharate2004@gmail.com>
2026-09-01 23:27:50 +0530

    Update app.js

--- Digital clock/app.js
+++ Digital clock/app.js
@@ -4,6 +4,6 @@ const time = document.querySelector("#time");
 setInterval(() =>{
     let date = new Date();
     console.log(date.toLocaleDateString());
-    // clock.innerText = date.toLocaleDateString();
+    clock.innerText = date.toLocaleDateString();
     time.innerHTML = date.toLocaleTimeString();
 // },1000);
